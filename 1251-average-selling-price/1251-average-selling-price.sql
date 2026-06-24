bc66b7495f9bd510cad33a7a03daedcf38cc781c
Rishabh2170 <rishu20378@gmail.com>
2026-06-24 23:51:12 +0530

    Time: 967 ms (19.13%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,11 @@
+
+select p.product_id ,
+        round(
+            coalesce(sum(us.units * p.price) *1.0 / sum(us.units) , 0) , 2
+        ) as average_price
+
+from prices p
+left join unitsSold us
+on p.product_id = us.product_id
+and  us.purchase_date between p.start_date and p.end_date
+group by p.product_id;
